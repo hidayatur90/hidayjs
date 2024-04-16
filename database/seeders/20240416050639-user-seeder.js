ff880f008db9704a108979b048ee1f10128108bf
hidayatur90 <hidabaik@gmail.com>
2024-04-16 12:35:29 +0700

    feat: seeder example

---
+++ database/seeders/20240416050639-user-seeder.js
@@ -0,0 +1,25 @@
+'use strict';
+const bcrypt = require('bcryptjs');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const hashedPassword = await bcrypt.hash('12345678', 10);
+    await queryInterface.bulkInsert('Users', [{
+      full_name: 'John Doe',
+      username: 'john77',
+      password: hashedPassword,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
