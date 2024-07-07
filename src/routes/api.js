0943ac6786ec7c2ae230d240bdbd9eaf765b4650
Osama-H <osama_29_@hotmail.com>
2024-07-07 21:54:59 +0300

    Optimize route authentication middleware usage

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,21 +1,22 @@
 // routes
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const UserController = require('../controllers/UserController');
-const AuthController = require('../controllers/auth/AuthController');
-const TokenController = require('../controllers/auth/TokenController');
+const UserController = require("../controllers/UserController");
+const AuthController = require("../controllers/auth/AuthController");
+const TokenController = require("../controllers/auth/TokenController");
 const authenticateToken = TokenController.authenticateToken;
 
 // Routes Auth
-router.post('/auth/register', AuthController.register);
-router.post('/auth/login', AuthController.login);
-router.post('/auth/logout', AuthController.logout);
+router.post("/auth/register", AuthController.register);
+router.post("/auth/login", AuthController.login);
+router.post("/auth/logout", AuthController.logout);
 
 // Routes User
-router.get('/user', authenticateToken, UserController.index);
-router.get('/user/:id', authenticateToken, UserController.show);
-router.post('/user', authenticateToken, UserController.store);
-router.put('/user/:id', authenticateToken, UserController.update);
-router.delete('/user/:id', authenticateToken, UserController.destroy);
+router.use(authenticateToken);
+router.get("/user", UserController.index);
+router.get("/user/:id", UserController.show);
+router.post("/user", UserController.store);
+router.put("/user/:id", UserController.update);
+router.delete("/user/:id", UserController.destroy);
 
 module.exports = router;
